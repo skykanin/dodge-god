c9fae3b88d03c104c83df88137b906144d3da99f
skykanin <3789764+skykanin@users.noreply.github.com>
2023-01-30 19:43:14 +0100

    clean up library

--- lib/Decrypt.hs
+++ lib/Decrypt.hs
@@ -1,16 +1,18 @@
 module Decrypt (decrypt) where
 
-import Codec.Compression.Zlib (decompress)
-import Data.ByteString.Char8 qualified as BS
-import Data.ByteString.Base64 qualified as B64 
-import Data.ByteString.Lazy qualified as BSL
+import Codec.Compression.Zlib qualified as Zlib
 import Data.Bits (xor)
-import Data.Either (fromRight)
-import Data.Word
+import Data.ByteString.Base64 qualified as B64
+import Data.ByteString.Char8 qualified as BS
+import Data.ByteString.Lazy qualified as BSL
+import Data.ByteString.Lazy.Char8 qualified as BSCL
+import Data.Word (Word8)
 
 decrypt :: Word8 -> String -> String
-decrypt key input = BS.unpack $ BSL.toStrict $ decompressed
-  where 
-    buf = BSL.fromStrict $ fromRight BS.empty $ B64.decode $ BS.pack input
-    decrypted = BSL.map (`xor` key) buf
-    decompressed = decompress decrypted
+decrypt key =
+  BSCL.unpack
+    . Zlib.decompress
+    . BSL.map (`xor` key)
+    . BSL.fromStrict
+    . B64.decodeLenient
+    . BS.pack
