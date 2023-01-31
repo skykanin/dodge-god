22071423b3e6592f3e197a8542b18ba55c6a613b
allmeta <thomasdasd@gmail.com>
2023-01-31 16:08:10 +0100

    index.js: format date and validate highscore

--- index.js
+++ index.js
@@ -46,25 +46,33 @@ function saveScores(){
 }
 
 function calculateScore(score){
-
   let date = new Date().toISOString()
-
   let notExists = !(score._name in scores[score._mode])
   let beatTime = score._name in scores[score._mode] && score._time > scores[score._mode][score._name][2]
   let isCorrectVersion = score._version == version
   let mode = score._mode
   let isCheating = false
 
-  // if ((notExists || beatTime) &&
-  //     isCorrectVersion &&
-  //     !isCheating) {
+  if ((notExists || beatTime) &&
+      isCorrectVersion &&
+      !isCheating) {
     let s = [score._name, score._time, date]
-    console.log(s)
     scores[mode][score._name] = s
     saveScores()
-  // }
-  let prepare = s => Object.values(s).sort(cmp).map(([n,t,d])=>[n,t,d.split("T")[0]])
+  }
+  let prepare = s => Object.values(s)
+                           .sort(cmp)
+                           .map(([n,t,d])=>[n,t,formatDate(d)])
+  console.log(prepare(scores.mouse))
   return {result:[prepare(scores.mouse), prepare(scores.keyboard), beatTime, isCorrectVersion, isCheating]}
+}
+
+let dates="Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ")
+
+let formatDate = d => {
+  let a = d.split("T")[0].split("-")
+  a[1] = dates[parseInt(a[1])-1]
+  return a.reverse().join(".")
 }
 
 function decrypt(d){
@@ -85,11 +93,12 @@ let j = {
     _name:"Karl",
     _time:10,
     _mode:"mouse",
-    _version:9,
+    _version:10,
     _dodge:2,
     _startTime:"19:54:24",
     _endTime:"19:54:28"
 }
+// calculateScore(j)
 
 app.listen(3000)
 console.log("listening on port 3000")
